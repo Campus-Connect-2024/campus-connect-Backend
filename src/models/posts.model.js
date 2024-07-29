a83e8bf38912f5a8ccaa80063fc9e541d1d64824
govind singh gurjar <gurjargovind994@gmail.com>
2024-07-29 23:11:56 +0530

    setup for project backend , included middleware and utils , multer , cloudinary

---
+++ src/models/posts.model.js
@@ -0,0 +1,40 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const postSchema= new Schema({
+    MediaFile: {
+        type: String, //cloudinary url
+        required: true
+    },
+    title: {
+        type: String, 
+        required: true
+    },
+    caption: {
+        type: String, 
+        required: true
+    },
+    duration: {
+        type: Number, 
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+},{timestamps:true}) 
+
+postSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Posts = mongoose.model("Posts",postSchema)
