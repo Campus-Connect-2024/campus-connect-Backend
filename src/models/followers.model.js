f3dea8b3aef15122c7d2d02b0e8e3a654a0bd33e
govind singh gurjar <gurjargovind994@gmail.com>
2024-07-31 12:28:23 +0530

    Added logout , login , register , refresh-token , change-password , current user , upadte account deatils, getUserProfile endpoint

---
+++ src/models/followers.model.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose"
+
+const followrsSchema = new Schema({
+    follower: {
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User"
+    },
+    owner: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+        ref: "User"
+    }
+}, {timestamps: true})
+
+
+
+export const Followers = mongoose.model("Followers", followrsSchema)
