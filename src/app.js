720082f6b713a501d05a9ed62799301427d66d2e
Govind Singh Gurjar <gurjargovind994@gmail.com>
2024-10-21 18:05:36 +0530

    added helemt.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import logger from "./utils/logger.js";
 import morgan from "morgan";
 import mongoSanitize from "express-mongo-sanitize";
-
+import helmet from "helmet";
 const app = express();
 
 const morganFormat = ":method :url :status :response-time ms";
@@ -37,6 +37,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(helmet());
 app.use(mongoSanitize());
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
